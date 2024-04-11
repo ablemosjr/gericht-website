657761908a3dd6997b6543f028e7810034da2abb
Alessandro B Lemos Jr <ablemosjunior@gmail.com>
2024-04-10 23:24:08 -0300

    feat: about/header container and subheading component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Navbar from './components/Navbar/Navbar';
+import Header from './container/Header/Header';
+import AboutUs from './container/AboutUs/AboutUs';
+
 import './App.css';
 
 const App = () => (
   <div>
     <Navbar />
+    <Header />
+    <AboutUs />
   </div>
-)
+);
 
-export default App
+export default App;
